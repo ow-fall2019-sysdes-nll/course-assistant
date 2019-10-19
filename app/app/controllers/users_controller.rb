bf3b54cc9c2119c0aaee76eb54f363acbf2d572b
Yufan Lou <2263580+louy2@users.noreply.github.com>
2019-10-18 22:12:29 -0400

    Add index show edit for User

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -1,9 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
   def index
-    @users = User.all()
+    @users = User.all
   end
 
+  # GET /users/1
   def show
-    @user = User.find(params[:id])
   end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name)
+    end
 end
