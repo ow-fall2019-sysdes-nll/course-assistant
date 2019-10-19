adb4b834a3abbb1f58e5e56ab93c737f3b841df9
Yufan Lou <2263580+louy2@users.noreply.github.com>
2019-10-18 22:12:01 -0400

    Add name to User

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_223113) do
+ActiveRecord::Schema.define(version: 2019_10_19_000234) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2019_10_18_223113) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
