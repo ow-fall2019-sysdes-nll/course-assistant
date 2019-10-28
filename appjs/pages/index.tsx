256661d83379010259b51a03e0dd061b436f2220
Yufan Lou <2263580+louy2@users.noreply.github.com>
2019-10-28 03:58:42 -0400

    Add typescript

---
+++ appjs/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h1>Welcome to ZLL University</h1>
+    <Link href="/academic-calendar">
+      <a>Academic Calendar</a>
+    </Link>
+  </div>
+);
